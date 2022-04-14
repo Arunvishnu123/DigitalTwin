473e6f0fb5b4e27765b4c05c6fae93a29074df5b
Arun R S <arunvishnu40@gmail.com>
2022-04-14 11:36:17 +0200

    3model display in vue.js is complete

--- smart-buidling-app/src/components/View3DModel.vue
+++ smart-buidling-app/src/components/View3DModel.vue
@@ -1,12 +1,15 @@
 <template>
 <div>
-    <router-link to="/" ><canvas id="myCanvas"></canvas></router-link>
+    <canvas id="myCanvas"></canvas>
+    <canvas id="NavCubeCanvas"></canvas>
+    <canvas id="AxisGizmoCanvas"></canvas>
 </div>
 </template>
 
 <script>
 import {
-    Viewer
+    Viewer,
+    NavCubePlugin,AxisGizmoPlugin
 } from "@xeokit/xeokit-sdk/";
 import * as display3d from "../features/view3D"
 
@@ -17,7 +20,19 @@ export default {
             canvasId: "myCanvas",
         });
         display3d.display3d(viewer)
-        
+
+        new NavCubePlugin(viewer, {
+            canvasId: "NavCubeCanvas",
+            // color: "lightblue",
+            visible: true, // Initially visible (default)
+            cameraFly: true, // Fly camera to each selected axis/diagonal
+            cameraFitFOV: 45, // How much field-of-view the scene takes once camera has fitted it to view
+            cameraFlyDuration: 0.5 // How long (in seconds) camera takes to fly to each new axis/diagonal
+        });
+        new AxisGizmoPlugin(viewer, {
+            canvasId: "AxisGizmoCanvas"
+        });
+        window.viewer = viewer;
     },
 };
 </script>
@@ -38,5 +53,21 @@ body {
     background-image: linear-gradient(lightblue, white);
 }
 
+#NavCubeCanvas {
+    position: absolute;
+    width: 250px;
+    height: 250px;
+    bottom: 100px;
+    left: 1300px;
+    z-index: 200000;
+}
 
+#AxisGizmoCanvas {
+    position: absolute;
+    width: 250px;
+    height: 250px;
+    bottom: 100px;
+    left: 50px;
+    z-index: 200000;
+}
 </style>
