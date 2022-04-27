adf28cc1b3e104c2306a7c9a399585584b4ad865
Arun R S <arunvishnu40@gmail.com>
2022-04-27 16:15:15 +0200

    new store added

--- smart-buidling-app/src/main.js
+++ smart-buidling-app/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from "./router/index";
 import store from './store/index';
+import store1 from './store/ReadMqtt'
 import WaveUI from 'wave-ui';
 import 'wave-ui/dist/wave-ui.css';
 //import 'font-awesome/css/font-awesome.min.css';
@@ -19,5 +20,6 @@ new Vue({
   router,
   store,
   waveui,
+  store1,
   render: (h) => h(App),
 }).$mount("#app");
---
+++ smart-buidling-app/src/store/ReadMqtt.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
+const store1 = new Vuex.Store({
+
+    state:{
+
+    },
+    actions:{
+        
+    }
+
+})
+
+export default store1;
